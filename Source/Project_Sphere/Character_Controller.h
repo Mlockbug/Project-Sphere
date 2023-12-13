3846416dfd72120abec6465bb0de1566a50b778c
Mlockbug <energetichamster@gmail.com>
2023-12-13 10:53:12 +0000

    test map

---
+++ Source/Project_Sphere/Character_Controller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Character_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SPHERE_API ACharacter_Controller : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
